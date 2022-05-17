09d42204571f8d19ee52407295f39025fb826e9c
Mario Luiz <maryoluyz@outlook.com>
2022-05-17 02:28:55 -0300

    H2 database

---
+++ backend/src/main/java/com/dvsup/dscatalog/services/CategoryService.java
@@ -0,0 +1,20 @@
+package com.dvsup.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dvsup.dscatalog.entities.Category;
+import com.dvsup.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}	
+}
